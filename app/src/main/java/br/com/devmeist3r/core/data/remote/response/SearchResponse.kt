bbb8708c6ba72e65e824ac242075f5eeddfa1724
luksinocencio <luksinocencio@gmail.com>
2023-11-10 16:17:26 -0300

    feat: implementar hilt

--- app/src/main/java/br/com/devmeist3r/core/data/remote/response/SearchResponse.kt
+++ app/src/main/java/br/com/devmeist3r/core/data/remote/response/SearchResponse.kt
@@ -1,8 +1,15 @@
 package br.com.devmeist3r.core.data.remote.response
 
-data class SearchResult(
+import br.com.devmeist3r.core.data.remote.model.SearchResult
+import com.google.gson.annotations.SerializedName
+
+data class SearchResponse(
+    @SerializedName("page")
     val page: Int,
-    val results: List<Result>,
-    val total_pages: Int,
-    val total_results: Int
+    @SerializedName("results")
+    val results: List<SearchResult>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
 )
