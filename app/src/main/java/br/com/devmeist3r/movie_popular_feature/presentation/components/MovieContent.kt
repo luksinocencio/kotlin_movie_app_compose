f04f8a6b039304785ddf90544d558298025a9fe1
luksinocencio <luksinocencio@gmail.com>
2023-12-02 02:15:50 -0300

    chore: remover import desnecessario

--- app/src/main/java/br/com/devmeist3r/movie_popular_feature/presentation/components/MovieContent.kt
+++ app/src/main/java/br/com/devmeist3r/movie_popular_feature/presentation/components/MovieContent.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
 import br.com.devmeist3r.core.domain.model.Movie
