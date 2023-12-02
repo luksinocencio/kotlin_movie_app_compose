80a176412c96f2b02696eae39cff96904c4b7231
luksinocencio <luksinocencio@gmail.com>
2023-12-02 02:13:09 -0300

    feat: implementar ViewModel e State

---
+++ app/src/main/java/br/com/devmeist3r/movie_popular_feature/presentation/MoviePopularViewModel.kt
@@ -0,0 +1,26 @@
+package br.com.devmeist3r.movie_popular_feature.presentation
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import br.com.devmeist3r.movie_popular_feature.domain.usecase.GetPopularMoviesUseCase
+import br.com.devmeist3r.movie_popular_feature.presentation.state.MoviePopularState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MoviePopularViewModel @Inject constructor(
+    getPopularMoviesUseCase: GetPopularMoviesUseCase
+): ViewModel() {
+    var uiState by mutableStateOf(MoviePopularState())
+        private set
+
+    init {
+        val movies = getPopularMoviesUseCase.invoke()
+            .cachedIn(viewModelScope)
+        uiState = uiState.copy(movies = movies)
+    }
+}
