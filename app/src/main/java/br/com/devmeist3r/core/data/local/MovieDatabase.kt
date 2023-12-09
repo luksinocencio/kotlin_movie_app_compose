617cd7f9b5ce73608d2093e7c798d4802fd1a5ca
luksinocencio <luksinocencio@gmail.com>
2023-12-09 03:28:10 -0300

    feat: implementar Entity, Dao, Database

---
+++ app/src/main/java/br/com/devmeist3r/core/data/local/MovieDatabase.kt
@@ -0,0 +1,15 @@
+package br.com.devmeist3r.core.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import br.com.devmeist3r.core.data.local.dao.MovieDao
+import br.com.devmeist3r.core.data.local.entity.MovieEntity
+
+@Database(
+    entities = [MovieEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class MovieDatabase: RoomDatabase() {
+    abstract fun movieDao(): MovieDao
+}
