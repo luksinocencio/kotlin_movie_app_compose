3000c9cb1ea3a41b2ad6b5e934a7b506393b6ae8
luksinocencio <luksinocencio@gmail.com>
2023-12-09 03:38:58 -0300

    feat: implementar di RoomModule

--- app/src/main/java/br/com/devmeist3r/core/util/Constants.kt
+++ app/src/main/java/br/com/devmeist3r/core/util/Constants.kt
@@ -1,6 +1,7 @@
 package br.com.devmeist3r.core.util
 
 object Constants {
+    const val MOVIE_DATABASE_NAME = "movie_db"
     const val MOVIE_DETAIL_ARGUMENT_KEY = "movieId"
     const val API_KEY_PARAM = "api_key"
     const val LANGUAGE_PARAM = "language"
