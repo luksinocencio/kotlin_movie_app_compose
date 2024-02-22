a3ae9e38765a7a388ee75f22628dccae77a8b316
luksinocencio <luksinocencio@gmail.com>
2024-02-21 21:10:40 -0300

    *

--- app/src/main/java/br/com/devmeist3r/MainActivity.kt
+++ app/src/main/java/br/com/devmeist3r/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import br.com.devmeist3r.core.presentation.MainScreen
 import br.com.devmeist3r.ui.theme.MovieAppTheme
@@ -19,6 +20,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MovieAppTheme {
                 MainScreen(navController = rememberNavController())
