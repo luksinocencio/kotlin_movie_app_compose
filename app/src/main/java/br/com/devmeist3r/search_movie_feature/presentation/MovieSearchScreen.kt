5160e6112b564fe81c62e833ea1f11453db88442
luksinocencio <luksinocencio@gmail.com>
2024-02-22 13:56:54 -0300

    feat: implementar component MovieAppBar

--- app/src/main/java/br/com/devmeist3r/search_movie_feature/presentation/MovieSearchScreen.kt
+++ app/src/main/java/br/com/devmeist3r/search_movie_feature/presentation/MovieSearchScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.paging.compose.collectAsLazyPagingItems
 import br.com.devmeist3r.R
+import br.com.devmeist3r.core.presentation.components.common.MovieAppBar
 import br.com.devmeist3r.search_movie_feature.presentation.components.SearchContent
 import br.com.devmeist3r.search_movie_feature.presentation.state.MovieSearchState
 import br.com.devmeist3r.ui.theme.black
@@ -15,42 +16,34 @@ import br.com.devmeist3r.ui.theme.white
 
 @Composable
 fun MovieSearchScreen(
-    uiState: MovieSearchState,
-    onEvent: (MovieSearchEvent) -> Unit,
-    onFetch: (String) -> Unit,
-    navigateToDetailMovie: (Int) -> Unit
+  uiState: MovieSearchState,
+  onEvent: (MovieSearchEvent) -> Unit,
+  onFetch: (String) -> Unit,
+  navigateToDetailMovie: (Int) -> Unit
 ) {
-    val pagingMovies = uiState.movies.collectAsLazyPagingItems()
+  val pagingMovies = uiState.movies.collectAsLazyPagingItems()
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = stringResource(id = R.string.search_movies),
-                        color = white
-                    )
-                },
-                backgroundColor = black
-            )
+  Scaffold(
+    topBar = {
+      MovieAppBar(title = R.string.search_movies)
+    },
+    content = { paddingValues ->
+      SearchContent(
+        paddingValues = paddingValues,
+        pagingMovies = pagingMovies,
+        query = uiState.query,
+        onSearch = {
+          onFetch(it)
         },
-        content = { paddingValues ->
-            SearchContent(
-                paddingValues = paddingValues,
-                pagingMovies = pagingMovies,
-                query = uiState.query,
-                onSearch = {
-                    onFetch(it)
-                },
-                onEvent = {
-                    onEvent(it)
-                },
-                onDetail = { movieId ->
-                    navigateToDetailMovie(movieId)
-                }
-            )
+        onEvent = {
+          onEvent(it)
+        },
+        onDetail = { movieId ->
+          navigateToDetailMovie(movieId)
         }
-    )
+      )
+    }
+  )
 }
 
 @Preview
