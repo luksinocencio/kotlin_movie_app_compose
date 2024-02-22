cc34961a7d2a9abda7abc91872dc98a8677dcede
luksinocencio <luksinocencio@gmail.com>
2024-02-22 14:09:28 -0300

    feat: implementar tratamento de lista vazia

--- app/src/main/java/br/com/devmeist3r/movie_favorite_feature/presentation/components/MovieFavoriteContent.kt
+++ app/src/main/java/br/com/devmeist3r/movie_favorite_feature/presentation/components/MovieFavoriteContent.kt
@@ -7,59 +7,76 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.devmeist3r.R
 import br.com.devmeist3r.core.domain.model.Movie
 import br.com.devmeist3r.ui.theme.black
+import br.com.devmeist3r.ui.theme.white
 
 @Composable
 fun MovieFavoriteContent(
-    modifier: Modifier = Modifier,
-    paddingValues: PaddingValues,
-    movies: List<Movie>,
-    onClick: (id: Int) -> Unit
+  modifier: Modifier = Modifier,
+  paddingValues: PaddingValues,
+  movies: List<Movie>,
+  onClick: (id: Int) -> Unit
 ) {
-    Box(modifier = modifier.background(black)) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            contentPadding = paddingValues,
-            content = {
-                items(
-                    items = movies,
-                    key = { item: Movie ->
-                        item.id
-                    }
-                ) { movie ->
-                    MovieFavoriteItem(movie = movie, onClick = onClick)
-                }
-            }
-        )
+  Box(modifier = modifier.background(black)) {
+    if (movies.isEmpty()) {
+      Text(
+        text = stringResource(id = R.string.favorite_movies_empty),
+        fontSize = 18.sp,
+        fontWeight = FontWeight.SemiBold,
+        color = white,
+        modifier = Modifier.align(Alignment.Center)
+      )
     }
+
+    LazyColumn(
+      modifier = Modifier.fillMaxSize(),
+      verticalArrangement = Arrangement.spacedBy(8.dp),
+      contentPadding = paddingValues,
+      content = {
+        items(
+          items = movies,
+          key = { item: Movie ->
+            item.id
+          }
+        ) { movie ->
+          MovieFavoriteItem(movie = movie, onClick = onClick)
+        }
+      }
+    )
+  }
 }
 
 @Preview
 @Composable
 fun MovieFavoriteContentPreview() {
-    MovieFavoriteContent(
-        modifier = Modifier,
-        paddingValues = PaddingValues(),
-        movies = listOf(
-            Movie(
-                id = 1,
-                title = "Homem Aranha",
-                voteAverage = 7.89,
-                imageUrl = ""
-            ),
-            Movie(
-                id = 2,
-                title = "Homem Ferro",
-                voteAverage = 7.89,
-                imageUrl = ""
-            )
-        ),
-        onClick = {}
-    )
+  MovieFavoriteContent(
+    modifier = Modifier,
+    paddingValues = PaddingValues(),
+    movies = listOf(
+      Movie(
+        id = 1,
+        title = "Homem Aranha",
+        voteAverage = 7.89,
+        imageUrl = ""
+      ),
+      Movie(
+        id = 2,
+        title = "Homem Ferro",
+        voteAverage = 7.89,
+        imageUrl = ""
+      )
+    ),
+    onClick = {}
+  )
 }
