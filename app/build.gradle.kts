8b29d9554ef985fdf3900ccb01f612b6c1e98fa6
luksinocencio <luksinocencio@gmail.com>
2024-02-27 21:51:29 -0300

    feat: implementar pegar todos os filmes; filmes ordenados pelo id

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,10 +2,10 @@ import java.util.Properties
 import java.io.FileInputStream
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.dagger.hilt.android")
-    id("kotlin-kapt")
+  id("com.android.application")
+  id("org.jetbrains.kotlin.android")
+  id("com.google.dagger.hilt.android")
+  id("kotlin-kapt")
 }
 
 val apiKeyPropertiesFile = rootProject.file("apiKey.properties")
@@ -13,136 +13,136 @@ val apiKeyProperties = Properties()
 apiKeyProperties.load(FileInputStream(apiKeyPropertiesFile))
 
 android {
-    namespace = "br.com.devmeist3r"
-    compileSdk = 34
+  namespace = "br.com.devmeist3r"
+  compileSdk = 34
 
-    defaultConfig {
-        applicationId = "br.com.devmeist3r"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+  defaultConfig {
+    applicationId = "br.com.devmeist3r"
+    minSdk = 24
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0"
 
 //        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        testInstrumentationRunner = "br.com.devmeist3r.HiltTestRunner"
+    testInstrumentationRunner = "br.com.devmeist3r.HiltTestRunner"
 
-        buildConfigField("String", "API_KEY", apiKeyProperties["API_KEY"] as String)
-        buildConfigField("String", "BASE_URL", apiKeyProperties["BASE_URL"] as String)
-        buildConfigField("String", "BASE_URL_IMAGE", apiKeyProperties["BASE_URL_IMAGE"] as String)
+    buildConfigField("String", "API_KEY", apiKeyProperties["API_KEY"] as String)
+    buildConfigField("String", "BASE_URL", apiKeyProperties["BASE_URL"] as String)
+    buildConfigField("String", "BASE_URL_IMAGE", apiKeyProperties["BASE_URL_IMAGE"] as String)
 
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+    vectorDrawables {
+      useSupportLibrary = true
     }
+  }
 
-    buildTypes {
+  buildTypes {
 
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures {
-        compose = true
-        buildConfig = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.2"
-    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlinOptions {
+    jvmTarget = "17"
+  }
+  buildFeatures {
+    compose = true
+    buildConfig = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.4.2"
+  }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+  implementation("androidx.core:core-ktx:1.12.0")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+  implementation("androidx.activity:activity-compose:1.8.0")
+  implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+  implementation("androidx.compose.ui:ui")
+  implementation("androidx.compose.ui:ui-graphics")
+  implementation("androidx.compose.ui:ui-tooling-preview")
+  implementation("androidx.compose.material3:material3")
+  testImplementation("junit:junit:4.13.2")
+  androidTestImplementation("androidx.test.ext:junit:1.1.5")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+  androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+  androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+  debugImplementation("androidx.compose.ui:ui-tooling")
+  debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    // Coil
-    implementation("io.coil-kt:coil-compose:2.5.0")
+  // Coil
+  implementation("io.coil-kt:coil-compose:2.5.0")
 
-    //Timber
-    implementation("com.jakewharton.timber:timber:5.0.1")
+  //Timber
+  implementation("com.jakewharton.timber:timber:5.0.1")
 
-    //DataStore
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+  //DataStore
+  implementation("androidx.datastore:datastore-preferences:1.0.0")
 
-    // Splashscreen
-    implementation("androidx.core:core-splashscreen:1.0.1")
+  // Splashscreen
+  implementation("androidx.core:core-splashscreen:1.0.1")
 
-    //Gson
-    implementation("com.google.code.gson:gson:2.10.1")
+  //Gson
+  implementation("com.google.code.gson:gson:2.10.1")
 
-    // Others - Compose dependencies
-    implementation("androidx.compose.material:material:1.3.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.compose.material:material-icons-extended:1.5.4")
-    implementation("com.google.accompanist:accompanist-flowlayout:0.17.0")
-    implementation("androidx.navigation:navigation-compose:2.7.5")
+  // Others - Compose dependencies
+  implementation("androidx.compose.material:material:1.3.1")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+  implementation("androidx.compose.material:material-icons-extended:1.5.4")
+  implementation("com.google.accompanist:accompanist-flowlayout:0.17.0")
+  implementation("androidx.navigation:navigation-compose:2.7.5")
 
-    // Paging3
-    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
-    implementation("androidx.paging:paging-compose:3.3.0-alpha02")
+  // Paging3
+  implementation("androidx.paging:paging-runtime-ktx:3.2.1")
+  implementation("androidx.paging:paging-compose:3.3.0-alpha02")
 
-    // Coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+  // Coroutines
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-    // Coroutine Lifecycle Scopes
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+  // Coroutine Lifecycle Scopes
+  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
-    // Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
-    implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
+  // Retrofit
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+  implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
+  implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
 
-    //DI - Hilt
-    implementation ("com.google.dagger:hilt-android:2.48.1")
-    implementation ("androidx.hilt:hilt-navigation-compose:1.1.0")
-    kapt ("com.google.dagger:hilt-compiler:2.48.1")
-    kapt ("androidx.hilt:hilt-compiler:1.1.0")
+  //DI - Hilt
+  implementation("com.google.dagger:hilt-android:2.48.1")
+  implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+  kapt("com.google.dagger:hilt-compiler:2.48.1")
+  kapt("androidx.hilt:hilt-compiler:1.1.0")
 
-    //Room
-    implementation("androidx.room:room-ktx:2.6.0")
-    implementation("androidx.room:room-runtime:2.6.0")
-    kapt ("androidx.room:room-compiler:2.6.0")
+  //Room
+  implementation("androidx.room:room-ktx:2.6.0")
+  implementation("androidx.room:room-runtime:2.6.0")
+  kapt("androidx.room:room-compiler:2.6.0")
 
-    testImplementation("junit:junit:4.13.2")
+  testImplementation("junit:junit:4.13.2")
 
   //truth
 
-  implementation("com.google.truth:truth:1.1.3")
-  androidTestImplementation("com.google.dagger:hilt-android-testing:2.45")
-  kaptAndroidTest("com.google.dagger:hilt-android-compiler:2.45")
+  implementation("com.google.truth:truth:1.4.0")
+  androidTestImplementation("com.google.dagger:hilt-android-testing:2.48")
+  kaptAndroidTest("com.google.dagger:hilt-android-compiler:2.48.1")
   androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
   androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
-  androidTestImplementation("com.google.truth:truth:1.1.3")
+  androidTestImplementation("com.google.truth:truth:1.4.0")
 
   // Dependência principal do Mockito
-  testImplementation ("org.mockito:mockito-core:2.28.2")
+  testImplementation("org.mockito:mockito-core:2.28.2")
   // Dependência do Mockito para testes no Android
   androidTestImplementation("org.mockito:mockito-android:2.28.2")
 
@@ -150,7 +150,7 @@ dependencies {
   testImplementation("org.mockito:mockito-inline:2.28.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-  androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.6.2")
+  androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.2")
   debugImplementation("androidx.compose.ui:ui-tooling:1.6.2")
   debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.2")
 }
--- app/src/androidTest/java/br/com/devmeist3r/data/local/dao/MovieDaoTest.kt
+++ app/src/androidTest/java/br/com/devmeist3r/data/local/dao/MovieDaoTest.kt
@@ -4,6 +4,7 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.filters.SmallTest
 import br.com.devmeist3r.core.data.local.MovieDatabase
 import br.com.devmeist3r.core.data.local.dao.MovieDao
+import br.com.devmeist3r.core.data.local.entity.MovieEntity
 import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -45,7 +46,7 @@ class MovieDaoTest {
   }
 
   @Test
-  fun getMovies() = runTest {
+  fun test_getMovies_should_return_list_of_movies() = runTest {
     // Given - nothing
 
     // When
@@ -53,5 +54,43 @@ class MovieDaoTest {
 
     // Then
     assertThat(movies.size).isEqualTo(0)
+  }
+
+  @Test
+  fun test_getMovies_should_return_movies_ordered_by_id() = runTest {
+    // Given
+    val moviesEntities = listOf(
+      MovieEntity(movieId = 1, title = "Homem de ferro 1", imageUrl = "any_url_1"),
+      MovieEntity(movieId = 2, title = "Homem de ferro 2", imageUrl = "any_url_2"),
+      MovieEntity(movieId = 3, title = "Homem de ferro 3", imageUrl = "any_url_3"),
+      MovieEntity(movieId = 4, title = "Homem de ferro 4", imageUrl = "any_url_4"),
+    )
+
+    movieDao.insertMovies(moviesEntities)
+
+    // When
+    val movies = movieDao.getMovies().first()
+
+    // Then
+    assertThat(movies.size).isEqualTo(4)
+    assertThat(movies[0].movieId).isEqualTo(1)
+    assertThat(movies[1].movieId).isEqualTo(2)
+    assertThat(movies[2].movieId).isEqualTo(3)
+    assertThat(movies[3].movieId).isEqualTo(4)
+  }
+
+  @Test
+  fun test_getMovies_should_return_correct_movie_by_id() = runTest {
+    // Given
+    val movieEntity = MovieEntity(movieId = 1, title = "Homem de ferro 1", imageUrl = "any_url_1")
+    movieDao.insertMovie(movieEntity)
+    val movies = movieDao.getMovies().first()
+    val movieClick = movies[0]
+
+    // When
+    val movieId = movieDao.getMovie(movieClick.movieId)
+
+    // Then
+    assertThat(movieId?.title).isEqualTo(movieClick.title)
   }
 }
